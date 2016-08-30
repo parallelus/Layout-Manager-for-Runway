0c5c9fea359b533dc27ebb09a936142cdc841710
Alexx G <alexxgermann@yandex.ru>
2016-08-30 15:39:00 +0300

    Refactoring for views/add-edit-footer.php

--- layout-manager/views/add-edit-footer.php
+++ layout-manager/views/add-edit-footer.php
@@ -1,24 +1,26 @@
 <?php global $shortname; ?>
-<form id="footer-add-edit" action="<?php echo $this->self_url(); ?>&navigation=footers-list&action=update-footer<?php echo isset($footer) ? '&old_alias='.$footer['alias'] : ''; ?>" method="post">
-	<input type="hidden" id="footer-alias" value="<?php echo isset($_GET['alias']) ? $_GET['alias'] : ''; ?>">
+<form id="footer-add-edit"
+      action="<?php echo $this->self_url(); ?>&navigation=footers-list&action=update-footer<?php echo isset( $footer ) ? '&old_alias=' . $footer['alias'] : ''; ?>"
+      method="post">
+	<input type="hidden" id="footer-alias" value="<?php echo isset( $_GET['alias'] ) ? $_GET['alias'] : ''; ?>">
 	<?php require_once "footer-form.php"; ?>
-	<input class="button-primary" type="button" id="save-button" value="<?php _e('Save Settings', 'runway') ?>">
+	<input class="button-primary" type="button" id="save-button" value="<?php _e( 'Save Settings', 'runway' ) ?>">
 </form>
 <script type="text/javascript">
-	(function($){
+	(function ($) {
 		$('.inside').css({'display': ''});
 
-		$('#save-button').click(function(e){
+		$('#save-button').click(function (e) {
 
 			var footerTitle = $('#footer-title').val().trim();
 			var prefix = '<?php echo $shortname . "layout_footer_"; ?>';
 
-			if(footerTitle == ''){
+			if (footerTitle == '') {
 				$('#footer-title').css('border-color', 'Red');
-			}
-			else{
+			} else {
 				var footer = $('#footer-alias').val();
-				if(footer == ''){
+
+				if (footer == '') {
 					$.ajax({
 						url: ajaxurl,
 						async: false,
@@ -26,12 +28,13 @@
 							action: 'sanitize_title',
 							string: footerTitle
 						}
-					}).done(function(responce){
-						footer = responce;
+					}).done(function (response) {
+						footer = response;
 						$('#footer-alias').val(footer);
 					});
 				}
-				save_custom_options(footer, prefix+footer, 'footer');
+
+				save_custom_options(footer, prefix + footer, 'footer');
 				$('#footer-add-edit').submit();
 			}
 		});
