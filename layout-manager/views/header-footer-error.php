a64afd2ed858c180da203fad9b03f2d0e45baf63
Alexx G <alexxgermann@yandex.ru>
2016-09-01 16:07:47 +0300

    Refactoring for views/header-footer-error.php

--- layout-manager/views/header-footer-error.php
+++ layout-manager/views/header-footer-error.php
@@ -1,18 +1,20 @@
 <?php
-if($header_err || $footer_err) { ?>
-<div class="updated">
-	<p>
-	<?php if($header_err)
-			_e( 'The header action needs to be placed at the very end of <strong>header.php</strong>. In most cases it will be the last thing in the file.', 'runway' );
+if ( $header_err || $footer_err ) { ?>
+	<div class="updated">
+		<p>
+			<?php if ( $header_err ) {
+				_e( 'The header action needs to be placed at the very end of <strong>header.php</strong>. In most cases it will be the last thing in the file.', 'runway' );
+			}
 
-          if($footer_err) {
-          	if($header_err)
-          		echo '<br>';
-    	 	_e( 'The footer action needs to be placed at the very end of <strong>footer.php</strong>. In most cases it will be the very first thing in the file.', 'runway' );
-    	  }
+			if ( $footer_err ) {
+				if ( $header_err ) {
+					echo '<br>';
+				}
+				_e( 'The footer action needs to be placed at the very end of <strong>footer.php</strong>. In most cases it will be the very first thing in the file.', 'runway' );
+			}
 
-		echo sprintf( __( ' See <a href="%s#header_footer_render"><strong>here</strong></a>.', 'runway' ), $this->self_url('settings') );
-	?>
-	</p>
-</div>
-<?php } ?>
+			echo sprintf( __( ' See <a href="%s#header_footer_render"><strong>here</strong></a>.', 'runway' ), $this->self_url( 'settings' ) );
+			?>
+		</p>
+	</div>
+<?php }
